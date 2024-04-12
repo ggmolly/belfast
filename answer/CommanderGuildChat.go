8b7a5906a2a9cf9b9ff1cd852244f514063ba14c
ggmolly <molly@mana.rip>
2024-04-12 22:37:23 +0200

    clean: initialize empty arrays for `CommanderGuildChat` & `CommanderGuildTechnologies`

--- answer/CommanderGuildChat.go
+++ answer/CommanderGuildChat.go
@@ -4,17 +4,9 @@ import (
 	"github.com/bettercallmolly/belfast/connection"
 
 	"github.com/bettercallmolly/belfast/protobuf"
-	"google.golang.org/protobuf/proto"
 )
 
-var validSC60101 protobuf.SC_60101
-
 func CommanderGuildChat(buffer *[]byte, client *connection.Client) (int, int, error) {
-	return client.SendMessage(60101, &validSC60101)
-}
-
-func init() {
-	data := []byte{}
-	panic("replayed packet: replace this with the actual data")
-	proto.Unmarshal(data, &validSC60101)
+	var response protobuf.SC_60101
+	return client.SendMessage(60101, &response)
 }
--- answer/CommanderGuildTechnologies.go
+++ answer/CommanderGuildTechnologies.go
@@ -4,17 +4,9 @@ import (
 	"github.com/bettercallmolly/belfast/connection"
 
 	"github.com/bettercallmolly/belfast/protobuf"
-	"google.golang.org/protobuf/proto"
 )
 
-var validSC62101 protobuf.SC_62101
-
 func CommanderGuildTechnologies(buffer *[]byte, client *connection.Client) (int, int, error) {
-	return client.SendMessage(62101, &validSC62101)
-}
-
-func init() {
-	data := []byte{}
-	panic("replayed packet: replace this with the actual data")
-	proto.Unmarshal(data, &validSC62101)
+	var response protobuf.SC_62101
+	return client.SendMessage(62101, &response)
 }
