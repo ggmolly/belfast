85eecfa448aaf72ab064405c273884fcb8349ce9
ggmolly <molly@mana.rip>
2024-04-12 22:59:14 +0200

    clean: initalize `MetaProgress` to a dummy value

--- answer/GetMetaProgress.go
+++ answer/GetMetaProgress.go
@@ -2,15 +2,14 @@ package answer
 
 import (
 	"github.com/bettercallmolly/belfast/connection"
+	"github.com/bettercallmolly/belfast/protobuf"
 
 	"google.golang.org/protobuf/proto"
 )
 
 func GetMetaProgress(buffer *[]byte, client *connection.Client) (int, int, error) {
-	return client.SendMessage(63315, &validSC63315)
-}
-
-func init() {
-	data := []byte{0x08, 0x01, 0x10, 0xc0, 0xac, 0xd0, 0x04}
-	proto.Unmarshal(data, &validSC63315)
+	response := protobuf.SC_63315{
+		Type: proto.Uint32(1),
+	}
+	return client.SendMessage(63315, &response)
 }
