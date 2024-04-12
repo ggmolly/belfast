b94fb464ebfea2264485af4c227ae099a5342c10
ggmolly <molly@mana.rip>
2024-04-12 23:00:54 +0200

    clean: send empty message for `GetMetaShipsPointsResponse`

--- answer/GetMetaShipsPointsResponse.go
+++ answer/GetMetaShipsPointsResponse.go
@@ -4,17 +4,9 @@ import (
 	"github.com/bettercallmolly/belfast/connection"
 
 	"github.com/bettercallmolly/belfast/protobuf"
-	"google.golang.org/protobuf/proto"
 )
 
-var validSC34002 protobuf.SC_34002
-
 func GetMetaShipsPointsResponse(buffer *[]byte, client *connection.Client) (int, int, error) {
-	return client.SendMessage(34002, &validSC34002)
-}
-
-func init() {
-	data := []byte{}
-	panic("replayed packet: replace this with the actual data")
-	proto.Unmarshal(data, &validSC34002)
+	var response protobuf.SC_34002
+	return client.SendMessage(34002, &response)
 }
