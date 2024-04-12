52cca74d2e6a7cc971c029228aa300749f281867
ggmolly <molly@mana.rip>
2024-04-12 22:34:15 +0200

    clean: initialize `TechnologyNationProxy` to empty arrays

--- answer/TechnologyNationProxy.go
+++ answer/TechnologyNationProxy.go
@@ -4,17 +4,9 @@ import (
 	"github.com/bettercallmolly/belfast/connection"
 
 	"github.com/bettercallmolly/belfast/protobuf"
-	"google.golang.org/protobuf/proto"
 )
 
-var validSC64000 protobuf.SC_64000
-
 func TechnologyNationProxy(buffer *[]byte, client *connection.Client) (int, int, error) {
-	return client.SendMessage(64000, &validSC64000)
-}
-
-func init() {
-	data := []byte{}
-	panic("replayed packet: replace this with the actual data")
-	proto.Unmarshal(data, &validSC64000)
+	var response protobuf.SC_64000
+	return client.SendMessage(64000, &response)
 }
