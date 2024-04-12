f1c832d770804841457a8c04eb04d62bf6ef21c8
ggmolly <molly@mana.rip>
2024-04-12 23:18:14 +0200

    rm: most of the deadcode

--- answer/CommanderCommissionsFleet.go
+++ answer/CommanderCommissionsFleet.go
@@ -7,17 +7,9 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-var validSC13201 protobuf.SC_13201
-
 func CommanderCommissionsFleet(buffer *[]byte, client *connection.Client) (int, int, error) {
 	var response protobuf.SC_13201
 	response.EliteExpeditionCount = proto.Uint32(0)
 	response.EscortExpeditionCount = proto.Uint32(0)
 	return client.SendMessage(13201, &response)
-}
-
-func init() {
-	data := []byte{}
-	panic("replayed packet: replace this with the actual data")
-	proto.Unmarshal(data, &validSC13201)
 }
