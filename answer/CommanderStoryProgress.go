e7e4de1295011b9f2a97239ca19b569ae0690da1
ggmolly <molly@mana.rip>
2024-04-12 23:22:17 +0200

    clean: `CommanderStoryProgress` to send an empty story progression

--- answer/CommanderStoryProgress.go
+++ answer/CommanderStoryProgress.go
@@ -7,14 +7,9 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-var validSC13001 protobuf.SC_13001
-
 func CommanderStoryProgress(buffer *[]byte, client *connection.Client) (int, int, error) {
-	return client.SendMessage(13001, &validSC13001)
-}
-
-func init() {
-	data := []byte{}
-	panic("replayed packet: replace this with the actual data")
-	proto.Unmarshal(data, &validSC13001)
+	response := protobuf.SC_13001{
+		DailyRepairCount: proto.Uint32(3),
+	}
+	return client.SendMessage(13001, &response)
 }
