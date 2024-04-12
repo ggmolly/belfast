40ab25adc9350b18bed4964795e8dcda28ab3f5d
ggmolly <molly@mana.rip>
2024-04-12 23:36:58 +0200

    rm: deadcode

--- answer/UNK_12101.go
+++ answer/UNK_12101.go
@@ -7,8 +7,7 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-var validSC12101 protobuf.SC_12101
-
+// ???
 func UNK_12101(buffer *[]byte, client *connection.Client) (int, int, error) {
 	var response protobuf.SC_12101
 	response.GroupList = append(response.GroupList, &protobuf.GROUPINFO{
@@ -16,10 +15,4 @@ func UNK_12101(buffer *[]byte, client *connection.Client) (int, int, error) {
 		ShipList: []uint32{307081, 405051, 901111, 605021, 805011, 603021},
 	})
 	return client.SendMessage(12101, &response)
-}
-
-func init() {
-	data := []byte{}
-	panic("replayed packet: replace this with the actual data")
-	proto.Unmarshal(data, &validSC12101)
 }
--- answer/UNK_13002.go
+++ answer/UNK_13002.go
@@ -7,16 +7,8 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-var validSC13002 protobuf.SC_13002
-
 func UNK_13002(buffer *[]byte, client *connection.Client) (int, int, error) {
 	var response protobuf.SC_13002
 	response.MaxTeam = proto.Uint32(0)
 	return client.SendMessage(13002, &response)
-}
-
-func init() {
-	data := []byte{}
-	panic("replayed packet: replace this with the actual data")
-	proto.Unmarshal(data, &validSC13002)
 }
