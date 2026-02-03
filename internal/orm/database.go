4c377c976a4200c4cac4f0a699aa5f25135c49a5
ggmolly <molly@molly.sh>
2026-02-03 15:27:09 +0100

    feat(ships): implement mod ship strengthening flow

--- internal/orm/database.go
+++ internal/orm/database.go
@@ -81,6 +81,7 @@ func seedDatabase(skipSeed bool) bool {
 		&OwnedShip{},
 		&OwnedShipEquipment{},
 		&OwnedShipTransform{},
+		&OwnedShipStrength{},
 		&OwnedSkin{},
 		&OwnedEquipment{},
 		&Punishment{},
