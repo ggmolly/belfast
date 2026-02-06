cadd0146cd3e43cbf08531222db855a7d4ac72f7
ggmolly <molly@molly.sh>
2026-02-06 19:21:02 +0100

    feat(packets): implement phantom quest finish

---
+++ internal/answer/finish_phantom_quest.go
@@ -0,0 +1,56 @@
+package answer
+
+import (
+	"github.com/ggmolly/belfast/internal/connection"
+	"github.com/ggmolly/belfast/internal/orm"
+	"github.com/ggmolly/belfast/internal/protobuf"
+	"google.golang.org/protobuf/proto"
+)
+
+func FinishPhantomQuest(buffer *[]byte, client *connection.Client) (int, int, error) {
+	var payload protobuf.CS_12210
+	if err := proto.Unmarshal(*buffer, &payload); err != nil {
+		return 0, 12211, err
+	}
+	response := protobuf.SC_12211{Result: proto.Uint32(0)}
+	shipID := payload.GetShipId()
+	shadowID := payload.GetSkinShadowId()
+	ship, ok := client.Commander.OwnedShipsMap[shipID]
+	if !ok {
+		response.Result = proto.Uint32(1)
+		return client.SendMessage(12211, &response)
+	}
+	quest, err := orm.GetTechnologyShadowUnlockConfig(shadowID)
+	if err != nil {
+		response.Result = proto.Uint32(1)
+		return client.SendMessage(12211, &response)
+	}
+	skinID, err := orm.GetShipBaseSkinIDTx(orm.GormDB, ship.ShipID)
+	if err != nil || skinID == 0 {
+		response.Result = proto.Uint32(1)
+		return client.SendMessage(12211, &response)
+	}
+
+	tx := orm.GormDB.Begin()
+	if tx.Error != nil {
+		response.Result = proto.Uint32(1)
+		return client.SendMessage(12211, &response)
+	}
+	if quest.Type == 5 {
+		if err := client.Commander.ConsumeResourceTx(tx, 4, quest.TargetNum); err != nil {
+			tx.Rollback()
+			response.Result = proto.Uint32(1)
+			return client.SendMessage(12211, &response)
+		}
+	}
+	if err := orm.UpsertOwnedShipShadowSkin(tx, client.Commander.CommanderID, shipID, shadowID, skinID); err != nil {
+		tx.Rollback()
+		response.Result = proto.Uint32(1)
+		return client.SendMessage(12211, &response)
+	}
+	if err := tx.Commit().Error; err != nil {
+		response.Result = proto.Uint32(1)
+		return client.SendMessage(12211, &response)
+	}
+	return client.SendMessage(12211, &response)
+}
