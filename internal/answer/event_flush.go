ed77d7d21713468c4fa47776f202dc8f0eb0dcf9
ggmolly <molly@molly.sh>
2026-02-07 13:14:05 +0100

    feat(answer): implement event collection flush

---
+++ internal/answer/event_flush.go
@@ -0,0 +1,45 @@
+package answer
+
+import (
+	"github.com/ggmolly/belfast/internal/connection"
+	"github.com/ggmolly/belfast/internal/orm"
+	"github.com/ggmolly/belfast/internal/protobuf"
+	"google.golang.org/protobuf/proto"
+)
+
+func EventFlush(buffer *[]byte, client *connection.Client) (int, int, error) {
+	var payload protobuf.CS_13009
+	if err := proto.Unmarshal(*buffer, &payload); err != nil {
+		return 0, 13010, err
+	}
+
+	var events []orm.EventCollection
+	if err := orm.GormDB.Where("commander_id = ?", client.Commander.CommanderID).Order("collection_id asc").Find(&events).Error; err != nil {
+		return 0, 13010, err
+	}
+
+	collectionList := make([]*protobuf.COLLECTIONINFO, 0, len(events))
+	for _, event := range events {
+		template, err := loadCollectionTemplate(event.CollectionID)
+		if err != nil {
+			return 0, 13010, err
+		}
+		overTime := uint32(0)
+		if template != nil {
+			overTime = template.OverTime
+		}
+
+		collectionList = append(collectionList, &protobuf.COLLECTIONINFO{
+			Id:         proto.Uint32(event.CollectionID),
+			FinishTime: proto.Uint32(event.FinishTime),
+			OverTime:   proto.Uint32(overTime),
+			ShipIdList: orm.ToUint32List(event.ShipIDs),
+		})
+	}
+
+	response := protobuf.SC_13010{
+		Result:         proto.Uint32(0),
+		CollectionList: collectionList,
+	}
+	return client.SendMessage(13010, &response)
+}
