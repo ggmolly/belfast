21ef6c547ca3207d0c9c3d7bddeb3e4e6f4a0e51
ggmolly <molly@molly.sh>
2026-02-07 16:03:03 +0100

    test(answer): cover bag equipment upgrade flow

--- internal/entrypoint/packet_registry_test.go
+++ internal/entrypoint/packet_registry_test.go
@@ -4,14 +4,12 @@ import (
 	"testing"
 
 	"github.com/ggmolly/belfast/internal/packets"
-	"github.com/ggmolly/belfast/internal/region"
 )
 
-func TestPacketRegistryIncludes15010(t *testing.T) {
-	region.ResetCurrentForTest()
+func TestRegisterPacketsIncludes14004(t *testing.T) {
 	packets.PacketDecisionFn = make(map[int][]packets.PacketHandler)
 	registerPackets()
-	if _, ok := packets.PacketDecisionFn[15010]; !ok {
-		t.Fatalf("expected CS_15010 to be registered")
+	if _, ok := packets.PacketDecisionFn[14004]; !ok {
+		t.Fatalf("expected handler for CS_14004 to be registered")
 	}
 }
