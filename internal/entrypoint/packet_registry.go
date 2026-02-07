e520d5d9cb68e7b358f1581cbe1e3a9a421b19d5
ggmolly <molly@molly.sh>
2026-02-07 12:25:10 +0100

    feat(answer): implement CS_18104 rival info

--- internal/entrypoint/packet_registry.go
+++ internal/entrypoint/packet_registry.go
@@ -76,6 +76,7 @@ func registerPackets() {
 	packets.RegisterPacketHandler(18001, []packets.PacketHandler{answer.ExerciseEnemies})
 	packets.RegisterPacketHandler(18100, []packets.PacketHandler{answer.GetArenaShop})
 	packets.RegisterPacketHandler(18102, []packets.PacketHandler{answer.RefreshArenaShop})
+	packets.RegisterPacketHandler(18104, []packets.PacketHandler{answer.GetRivalInfo})
 	packets.RegisterPacketHandler(60033, []packets.PacketHandler{answer.GetGuildShop})
 	packets.RegisterPacketHandler(16106, []packets.PacketHandler{answer.GetMedalShop})
 	packets.RegisterPacketHandler(60037, []packets.PacketHandler{answer.CommanderGuildData})
