a2e6109444d6c51872eaee25123618b13e105491
ggmolly <molly@molly.sh>
2026-02-07 16:00:11 +0100

    feat(answer): send chapter base sync on login

---
+++ internal/answer/chapter_base_sync.go
@@ -0,0 +1,33 @@
+package answer
+
+import (
+	"errors"
+
+	"github.com/ggmolly/belfast/internal/connection"
+	"github.com/ggmolly/belfast/internal/orm"
+	"github.com/ggmolly/belfast/internal/protobuf"
+	"google.golang.org/protobuf/proto"
+	"gorm.io/gorm"
+)
+
+func ChapterBaseSync(_ *[]byte, client *connection.Client) (int, int, error) {
+	response := protobuf.SC_13000{
+		DailyRepairCount: proto.Uint32(0),
+	}
+
+	state, err := orm.GetChapterState(orm.GormDB, client.Commander.CommanderID)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return client.SendMessage(13000, &response)
+		}
+		return 0, 13000, err
+	}
+
+	var current protobuf.CURRENTCHAPTERINFO
+	if err := proto.Unmarshal(state.State, &current); err != nil {
+		return 0, 13000, err
+	}
+	response.CurrentChapter = &current
+
+	return client.SendMessage(13000, &response)
+}
